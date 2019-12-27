bc2d16243b40ea5e08b8821c8fd6ddc88bc9e896
Leon van der Noll <leon.vandernoll@onegini.com>
2019-12-27 22:18:56 +0100

    implemented more backend

--- src/pages/setCards/components/card.js
+++ src/pages/setCards/components/card.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus, faMinus, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faMinus, faSpinner, faHandPaper, faHandPointLeft } from '@fortawesome/free-solid-svg-icons';
 import styles from './card.module.scss';
 
 class Card extends Component {
   constructor(props) {
     super(props);
 
-    this.card = props.card;
-
     this.state = {
-      count: 0,
+      card: this.props.card,
+      count: this.props.card.total_cards,
       detail: false,
+      detailedImgLoaded: false,
       detailLoaded: false,
     };
+  }
+
+  action = actionName => {
+    fetch(`${process.env.REACT_APP_API_URL}/set/${this.props.set}/${this.state.card.id}/`, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${this.props.user.data.token}`,
+      },
+      body: JSON.stringify({ 'action': actionName }),
+    })
+    .then(response => {
+      if(response.status === 204) {
+        switch(actionName) {
+          case 'add': 
+            this.setState({ count: this.state.count + 1 });
+            break;
+          case 'remove':
+          case 'loan':
+            this.setState({ count: this.state.count - 1 });
+            break;
+          default:
+        }
+      }
+      else response.json().then(responseJson => console.error(responseJson));
+    })
+    .catch(e => console.error(e));
   }
 
   addCard = () => {
@@ -24,29 +53,64 @@ class Card extends Component {
     if(this.state.count !== 0) this.setState({ count: this.state.count - 1 });
   }
 
-  toggleDetail = () => this.setState({ detail: !this.state.detail });
+  openDetail = () => {
+    this.setState({ detail: !this.state.detail });
+    fetch(`${process.env.REACT_APP_API_URL}/card/${this.state.card.id}/`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Token ${this.props.user.data.token}`,
+      },
+    })
+    .then(response => response.json().then(responseJson => {
+      if(response.status === 200) this.setState({ detailLoaded: true, card: {...this.state.card, ...responseJson} });
+      else console.error(responseJson);
+    }))
+    .catch(e => console.error(e));
+  }
 
-  onDetailLoad = () => this.setState({ detailLoaded: true });
+  closeDetail = e => e.target === this.refs.popup && this.setState({ detail: !this.state.detail });
+
+  onDetailLoad = () => this.setState({ detailedImgLoaded: true });
 
   render = () => {
-    const card = this.card;
-    const { count }= this.state;
+    const { card, count, detailLoaded } = this.state;
 
     return (
       <>
         <article className={`${styles.card} ${count === 0 ? styles.grey : ''}`}>
-          <img className={styles.image} alt='' src={card.imageUrl} onClick={this.toggleDetail} />
+          <img ref='img' className={styles.image} alt='' src={card.imageUrl} onClick={this.openDetail} />
           <div className={styles.count}>{count}</div>
-          {count !== 0 && <FontAwesomeIcon className={`${styles.button} ${styles.buttonLeft}`} icon={faMinus} onClick={this.removeCard} />}
-          <FontAwesomeIcon className={`${styles.button} ${styles.buttonRight}`} icon={faPlus} onClick={this.addCard} />
+          {count !== 0 && <FontAwesomeIcon className={`${styles.button} ${styles.buttonLeft}`} icon={faMinus} onClick={() => this.action('remove')} />}
+          <FontAwesomeIcon className={`${styles.button} ${styles.buttonRight}`} icon={faPlus} onClick={() => this.action('add')} />
         </article>
-        {this.state.detail && 
-          <div className={styles.detailWrapper} onClick={this.toggleDetail}>
-            {!this.state.detailLoaded && <div className={styles.spinnerWrapper}><FontAwesomeIcon className={styles.spinner} icon={faSpinner} /></div>}
-            <img className={`${styles.detailImg} ${this.state.detailLoaded ? '' : styles.hidden}`} src={card.imageUrlHiRes} alt='detail' onLoad={this.onDetailLoad} />
+        {this.state.detail &&
+          <div ref='popup' className={styles.detailWrapper} onClick={this.closeDetail}>
+            <div className={styles.detailImgWrapper}>
+              {!this.state.detailedImgLoaded && <div className={styles.spinnerWrapper}><FontAwesomeIcon className={styles.spinner} icon={faSpinner} /></div>}
+              <img className={`${styles.detailImg} ${this.state.detailedImgLoaded ? '' : styles.hidden}`} src={card.imageUrlHiRes} alt='detail' onLoad={this.onDetailLoad} />
+              {count !== 0 && <FontAwesomeIcon className={`${styles.button} ${styles.buttonBig} ${styles.buttonBigLeft}`} icon={faHandPaper} onClick={() => this.action('loan')} />}
+              <FontAwesomeIcon className={`${styles.button} ${styles.buttonBig} ${styles.buttonBigRight}`} icon={faHandPointLeft} onClick={() => this.action('hand-in')} />
+            </div>
             <div className={styles.detailInfo}>
-              <span>info</span>
-              <span>info</span>
+              <ul>
+                <li>Name: {card.name}</li>
+                <li>Number: {card.number}</li>
+                <li>Amount: {card.total_cards}</li>
+                {card.types && <li>Types: {card.types.join(', ')}</li>}
+                <li>Supertype: {card.supertype}</li>
+                <li>Rarity: {card.rarity}</li>
+              </ul>
+              {detailLoaded && <div className={styles.actionWrapper}>
+                <span className={styles.actionTitle}>Actions</span>
+                <ul>
+                  {card.actions.map((action, i) => <ul key={i} className={styles.actionItem}>
+                    <li>Name: {action.name}</li>
+                    <li>Action: {action.action}</li>
+                    <li>Timestamp: {action.timestamp}</li>
+                  </ul>)}
+                </ul>
+              </div>}
             </div>
           </div>
         }
@@ -55,4 +119,4 @@ class Card extends Component {
   }
 };
 
-export default Card;
+export default connect(state => ({ user: state.user }))(Card);
--- src/pages/setCards/index.js
+++ src/pages/setCards/index.js
@@ -1,33 +1,67 @@
 import React, { Component } from 'react';
-import styles from './setCards.module.scss';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Card from './components/card';
+import styles from './setCards.module.scss';
 
 class SetCardsPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isLoading: false,
       cards: [],
+      set: 0,
     };
   }
 
-  componentDidMount = () => {
+  componentDidMount = async () => {
+    let cards;
     try {
-      const cards = require(`data/${this.props.location.pathname.split('/set/').join('')}.json`);
-      if(cards) this.setState({ cards: cards.cards });
+      cards = await require(`data/${this.props.location.pathname.split('/set/').join('')}.json`);
+      if(cards) cards = cards.cards;
     } catch (e) {
       this.props.history.goBack();
     }
+
+    this.setState({ isLoading: true });
+    fetch(`${process.env.REACT_APP_API_URL}/set/2`, { // CHANGE THIS ID TO A NAME EVENTUALLY
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': `Token ${this.props.user.data.token}`,
+      },
+    })
+    .then(response => response.json().then(responseJson => {
+      if(response.status === 200) {
+        this.setState({ isLoading: false, set: responseJson.id });
+        const newCards = [];
+        responseJson.cards.forEach(card => {
+          // eslint-disable-next-line
+          const sameCard = cards.find(obj => obj.number == card.number);
+          if(sameCard) newCards.push({...card, ...sameCard});
+        });
+        this.setState({ cards: newCards });
+      }
+      else {
+        console.error(responseJson);
+        this.setState({ isLoading: false });
+      }
+    }))
+    .catch(e => {
+      console.error(e);
+      this.setState({ isLoading: false });
+    });
   }
 
   render = () => {
-    const { cards } = this.state;
+    const { cards, set } = this.state;
 
     return (
       <section className={styles.page}>
-        {cards.map((card, i) => <Card key={i} card={card} />)}
+        {cards.map((card, i) => <Card key={i} card={card} set={set} />)}
       </section>
     );
   }
 }
 
-export default SetCardsPage;
+export default withRouter(connect(state => ({ user: state.user }))(SetCardsPage));
